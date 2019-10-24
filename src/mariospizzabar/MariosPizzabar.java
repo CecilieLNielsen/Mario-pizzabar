ee06f4675d8503fd9ae2c81cf2b109cc544c1716
Cecilie <cecilienielsen@live.dk>
2019-10-24 13:54:47 +0200

    Updated Menukort

---
+++ src/mariospizzabar/MariosPizzabar.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mariospizzabar;
+
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Cecilie
+ */
+public class MariosPizzabar {
+    public static void main(String[] args) {
+        Menukort bestillingsliste = new Menukort();
+        try {
+            bestillingsliste.opretMenukort();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(MariosPizzabar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+}
---
+++ src/mariospizzabar/Menukort.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mariospizzabar;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Cecilie
+ */
+public class Menukort {
+
+   ArrayList<Pizza> menukort = new ArrayList<Pizza>();
+
+
+    public void opretMenukort() throws FileNotFoundException {
+        File fil = new File("Menukort.txt");
+        Scanner sc = new Scanner(fil);
+
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] details = line.split(" ");
+            int nummer = Integer.parseInt(details[0].replace(".", ""));
+            String navn = details[1].replace(":", "");
+            String[] fyld = details[2].split(",");
+            int pris = Integer.parseInt(details[3].replace(",-", ""));
+            boolean nyhed = false;
+            if (details.length == 5) {
+                nyhed = true;
+
+                Pizza p = new Pizza(nummer, navn, fyld, pris, nyhed);
+                menukort.add(p);
+            }
+
+            for (Pizza p : menukort) {
+                System.out.println(p.toString());
+
+            }
+
+        }
+
+    }
+}
